9f7f689f477d44d5b5d4fc277c689cb7c89b3548
vfadeev <vfadeevmail@gmail.com>
2022-05-25 18:14:12 +0400

    Initial commit

---
+++ src/test/java/com/company/bankingnewcustomeronboardingvtb/BankingNewCustomerOnboardingVTBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.bankingnewcustomeronboardingvtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingNewCustomerOnboardingVTBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
